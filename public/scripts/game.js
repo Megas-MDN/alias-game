26477ae7771b4aa054dab2b7d2ae7fba266b1cc4
megas-mdn <mdn.melqui@gmail.com>
2024-10-10 13:15:41 -0300

    feat: GAme chat stuffs

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,6 +1,9 @@
 const exitBtn = document.getElementById("exitBtn");
 const playerName = document.getElementById("playerName");
 const messagesList = document.getElementById("messages");
+const guessContainer = document.getElementById("guessContainer");
+const messageInput = document.getElementById("messageInput");
+const guessInput = document.getElementById("guessInput");
 
 const socket = io("/");
 let user = null;
@@ -10,10 +13,23 @@ document.getElementById("backBtn").onclick = () => {
   window.location.href = "/";
 };
 
+guessInput.addEventListener("input", function () {
+  this.value = this.value.replace(/\s/g, "");
+});
+
+const setInLocalStorage = (key, value) => {
+  localStorage.removeItem(key);
+  const str = JSON.stringify(value);
+  localStorage.setItem(key, str);
+  return value;
+};
+
 const clearAll = () => {
   localStorage.removeItem("user");
   localStorage.removeItem("game");
   localStorage.removeItem("token");
+  localStorage.removeItem("gameDetails");
+  localStorage.clear();
 };
 exitBtn.onclick = () => {
   clearAll();
@@ -89,21 +105,61 @@ const fetchGameMessages = async (gameId, teamId) => {
   return data;
 };
 
+const loadMessages = async () => {
+  if (!user || !game) {
+    return;
+  }
+  const data = await fetchGameMessages(game.gameId, game.teamId);
+  (data || []).forEach((chat) => {
+    createNewLine({
+      ...chat,
+      username: chat?.userId.username,
+      userId: chat?.userId._id,
+    });
+  });
+};
+
+const fetchGameDetails = async () => {
+  if (!game || !game.gameId) return;
+  const response = await fetch(`/api/games/${game.gameId}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+    },
+  });
+  const data = await response.json();
+  if (data) {
+    const gameDetails = {
+      teamIdTurn: data?.currentTurnTeam,
+      userIdDescriber: data.currentDescriber,
+      status: data?.status,
+    };
+    setInLocalStorage("gameDetails", gameDetails);
+    return gameDetails;
+  }
+  return data;
+};
+
+const setGameDetails = async () => {
+  const data = await fetchGameDetails();
+
+  if (user.id === data.userIdDescriber) {
+    console.log("describer");
+    guessContainer.remove();
+    messageInput.placeholder = "Describe the word";
+  }
+};
+
 window.onload = () => {
   user = getUserFromLocalStorage();
   game = getGameFromLocalStorage();
   if (!user || !game) {
     clearAll();
     window.location.replace("/login.html");
+    return;
   }
   playerName.textContent = user.username;
-  fetchGameMessages(game.gameId, game.teamId).then((data) => {
-    (data || []).forEach((chat) => {
-      createNewLine({
-        ...chat,
-        username: chat?.userId.username,
-        userId: chat?.userId._id,
-      });
-    });
-  });
+  loadMessages();
+  setGameDetails();
 };
