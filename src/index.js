e38f4779a32e4b906c84790dec22b71407af77b7
magali <maguimourino@gmail.com>
2024-10-13 15:47:03 -0300

    add db game & users seed

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const http = require("http");
 const socketSetup = require("../src/sockets/socket");
+const seedDatabase = require("../src/utils/seed"); 
 
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI;
 const app = require("../src/app");
 
 // Connect MongoDB
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
+const startServer = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
     console.log("Connected to MongoDB");
+
+    await seedDatabase();  //call seedDatabase function
+
     const server = http.createServer(app);
     socketSetup(server);
     server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  })
-  .catch((err) => {
+  } catch (err) {
     console.error("Error connecting to MongoDB:", err);
-  });
+  }
+};
+
+// start server
+startServer();
