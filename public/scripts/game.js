33ce693d09d2f8da194603a947e54ef1ab18ab61
megas-mdn <mdn.melqui@gmail.com>
2024-10-15 09:12:31 -0300

    fix: Toggle turn in the time up

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -180,9 +180,9 @@ socket.on("turnChanged", async (data) => {
   }
 });
 
-socket.on("changeTurn", (data) => {
-  if (data.gameId === game.gameId) toggleTurn();
-});
+// socket.on("changeTurn", (data) => {
+//   if (isUserDescribe && data.gameId === game.gameId) toggleTurn();
+// });
 
 const drawEndGame = (message, isTie = false) => {
   const finishedGameDiv = document.getElementById("finishedGame");
@@ -427,7 +427,8 @@ const startTimer = () => {
       } else {
         clearInterval(timerInterval);
         if (isUserDescribe) {
-          socket.emit("goChangeTurn", { gameId: game.gameId });
+          // socket.emit("goChangeTurn", { gameId: game.gameId });
+          toggleTurn();
         }
       }
     }, 1000);
